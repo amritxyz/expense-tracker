b8b5f25c8ad21e98fc6ad35582813f83b8b38053
susma33 <susmatandan23@gmail.com>
2025-09-22 01:00:03 +0545

    Just accept the merge request * Working on nothing (Dashboard) -> Navbar (not completed)

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,14 @@
+import Navbar_auth from "./Navbar_auth";
+
 export default function Dashboard() {
   return (
     <>
-      Hell yeah!
+      {/*
+        * INFO: Navbar is useless using just using in development phase
+        *       It's so pretty just like me..😉
+        * TODO: A vertical Navbar...
+        */}
+      <Navbar_auth />
     </>
   );
 }
